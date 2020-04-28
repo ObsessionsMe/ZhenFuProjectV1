a36347568fe4c77aed2e3a7dc35efee7272953c4
86159 <86159@DESKTOP-EONNA4V>
2020-04-28 18:22:30 +0800

    分页帮助类

---
+++ WebApi/WebUI/Tool/PaginationParam.cs
@@ -0,0 +1,14 @@
+﻿
+using Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebUI.Tool
+{
+   public  class PaginationParam
+    {
+        public Pagination pagination { get; set; }
+        public string keyword { set; get; }
+    }
+}
