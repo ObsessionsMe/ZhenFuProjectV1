d34664775c9d2822c2316bbc5e742047e6fc23ca
86159 <86159@DESKTOP-EONNA4V>
2020-04-28 18:29:27 +0800

    2

---
+++ WebApi/BusinessLogic/ManageService/GoodsManageServices.cs
@@ -0,0 +1,30 @@
+﻿using Entity;
+using Infrastructure;
+using RepositoryFactory.ServiceInterface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogic.ManageService
+{
+   public class GoodsManageServices
+    {
+        /// <summary>
+        ///  商品模块业务逻辑
+        /// </summary>
+        private IGoodsRepository goodsRepository;
+        public GoodsManageServices(IGoodsRepository _goodsRepository)
+        {
+            goodsRepository = _goodsRepository;
+        }
+        public List<GoodsEntity> GetGoodsList(Pagination pagination, string keyword)
+        {
+            var expression = ExtLinq.True<GoodsEntity>();
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                expression = expression.And(t => t.GoodsName.Contains(keyword));
+            }
+            return goodsRepository.FindList(expression, pagination);
+        }
+    }
+}
---
+++ WebApi/BusinessLogic/ManageService/UserManageService.cs
@@ -0,0 +1,43 @@
+﻿using Entity;
+using Infrastructure;
+using RepositoryFactory.ServiceInterface;
+using System.Collections.Generic;
+
+namespace BusinessLogic.ManageService
+{
+    /// <summary>
+    /// 商城管理后台: 用户模块业务逻辑
+    /// </summary>
+    public class UserManageService
+    {
+        private IUserRepository userRepository;
+        public UserManageService(IUserRepository _userRepository)
+        {
+            userRepository = _userRepository;
+        }
+        /// <summary>
+        ///  登录
+        /// </summary>
+        /// <param name="telephone"></param>
+        /// <param name="password_r"></param>
+        /// <returns></returns>
+        public UserInfoEntity CheckLogin(string userId, string password)
+        {
+            return userRepository.FindEntity(x => x.UserId == userId && x.Enable == "Y" && x.Password == password && x.IsAdmin == "Y");
+        }
+        public UserInfoEntity GetUserByUserId(string userId)
+        {
+            return userRepository.FindEntity(x => x.UserId == userId && x.Enable == "Y" && x.IsAdmin == "Y");
+        }
+
+        public List<UserInfoEntity> GetUserList(Pagination pagination, string keyword)
+        {
+            var expression = ExtLinq.True<UserInfoEntity>();
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                expression = expression.And(t => t.Name.Contains(keyword));
+            }
+            return userRepository.FindList(expression, pagination);
+        }
+    }
+}
