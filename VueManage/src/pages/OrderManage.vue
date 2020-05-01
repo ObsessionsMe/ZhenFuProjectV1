a36df58f2adcd477776efe697360eba2d1933224
86159 <86159@DESKTOP-EONNA4V>
2020-05-01 23:19:05 +0800

    订单

---
+++ VueManage/src/pages/OrderManage.vue
@@ -0,0 +1,155 @@
+<template>
+  <div class="container">
+    <el-form :inline="true" class="demo-form-inline" style="text-align:left">
+      <el-form-item label="订单编号">
+        <el-input placeholder="请输入订单编号"></el-input>
+      </el-form-item>
+      <el-form-item label="用户手机号">
+        <el-input placeholder="请输入用户手机号"></el-input>
+      </el-form-item>
+      <el-form-item label="订单状态">
+        <el-select placeholder="请选择订单状态" v-model="defalutOrderType" >
+          <el-option
+            v-for="item in allOrderType"
+            :key="item.id"
+            :label="item.name"
+            :value="item.id"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" icon="el-icon-circle-plus-outline" @click="searchOrderList">查询</el-button>
+        <!-- <el-button type="primary" icon="el-icon-edit" @click="editUserGroup">编辑</el-button>
+        <el-button type="primary" icon="el-icon-delete" @click="deleteUserGroup">删除</el-button> -->
+        <el-button type="primary" icon="el-icon-search" @click="exportOrderList">批量导入快递单号(excel)</el-button>
+      </el-form-item>
+    </el-form>
+    <section class="content">
+      <h5>订单管理</h5>
+      <br />
+      <el-row>
+        <el-table
+          :data="tableData"
+          tooltip-effect="dark"
+          style="width: 100%"
+          highlight-current-row
+          height="650"
+        >
+          <el-table-column prop="goodsId"   label="用户姓名" sortable width="100"></el-table-column>
+          <el-table-column prop="goodsId"   label="用户手机号" sortable width="150"></el-table-column>
+          <el-table-column prop="goodsLevel" label="订单编号" width="150"></el-table-column>
+          <el-table-column prop="goodsLevel" label="商品名称" width="200"></el-table-column>
+          <el-table-column prop="unitPrice"  label="商品类型" sortable width="100"></el-table-column>
+          <el-table-column prop="itemPoints" label="购买商品数量" sortable width="150"></el-table-column>
+          <el-table-column prop="directPoints" label="订单总金额" sortable width="150"></el-table-column>
+          <el-table-column prop="enable" label="快递单号" sortable width="200"></el-table-column>
+          <el-table-column prop="enable" label="支付方式" sortable width="100"></el-table-column>
+          <el-table-column prop="enable" label="使用积分类型" sortable width="150"></el-table-column>
+         <el-table-column prop="indirectPoints" label="订单状态" sortable width="100"></el-table-column>
+        <el-table-column prop="indirectPoints" label="下单时间" sortable width="200"></el-table-column>
+        </el-table>
+      </el-row>
+      <el-pagination
+        background
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="pageIndex"
+        :page-sizes="[10, 15, 20, 30, 50, 100]"
+        :page-size="pageSize"
+        layout="total, sizes, prev, pager, next"
+        :total="total"
+      ></el-pagination>
+    </section>
+  </div>
+</template>
+
+<script>
+//此处引入
+import { http, url } from "@/lib";
+//我的存储
+export default {
+  data() {
+    return {
+      kw: "",
+      tableData: [],
+      total: 0,
+      pageSize: 10,
+      pageIndex: 1,
+      keyword: "",
+      defalutOrderType:100,
+      allOrderType: [
+        { id: 100,name: "全部" },
+        { id: 0, name: "待付款" },
+        { id: 1, name: "待发货" },
+        { id: 2, name: "待收货" },
+        { id: 3, name: "已完成" }
+      ],
+    };
+  },
+  created() {
+    //页面初始化
+    //this.searchApplyList();
+  },
+  methods: {
+    //获取用户兑换申请列表
+    searchApplyList() {
+      http
+        .post(url.GetGoodsList, {
+          pagination: {
+            rows: this.pageSize,
+            page: this.pageIndex,
+            sidx: "Id",
+            sord: "desc",
+            record: ""
+          },
+          keyword: this.kw
+        })
+        .then(res => {
+          console.log("res", res)
+          this.tableData = res.data.data.rows;
+          this.total = res.data.data.records;
+        });
+    },
+    handleSizeChange(currentsize) {
+      this.pageSize = currentsize;
+      this.pageIndex = 1;
+      this.searchApplyList();
+    },
+    handleCurrentChange(currentindex) {
+      this.pageIndex = currentindex;
+      this.searchApplyList();
+    },
+    //查看商品
+    exportApplyList(){
+
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  overflow: auto;
+}
+
+.el-button-group {
+  margin-right: 10px;
+}
+
+.toolBar {
+  height: 60px;
+  justify-content: flex-start;
+  align-items: center;
+  display: flex;
+}
+
+.content {
+  text-align: left;
+  flex: 1;
+}
+</style>
