23468e4f956a67e4a96a9eacfd7ea6dff530c391
86159 <86159@DESKTOP-EONNA4V>
2020-05-04 22:51:23 +0800

    。。。

---
+++ WebApi/Repository/RepositoryService/AttachMentRepository.cs
@@ -0,0 +1,18 @@
+﻿using Entity;
+using Infrastructure.DBContext;
+using RepositoryFactory.RepositorysBase;
+using RepositoryFactory.ServiceInterface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.RepositoryService
+{
+    public class AttachMentRepository : Repository<AttachMentInfoEntity>, IAttachMentRepository
+    {
+        public AttachMentRepository(ZFDBContext _dbcontext) : base(_dbcontext)
+        {
+
+        }
+    }
+}
