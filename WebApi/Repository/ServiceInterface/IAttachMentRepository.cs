1551983becdab8543d95878eb0c285b1977c2a84
86159 <86159@DESKTOP-EONNA4V>
2020-05-04 22:52:20 +0800

    。。。。

---
+++ WebApi/Repository/ServiceInterface/IAttachMentRepository.cs
@@ -0,0 +1,13 @@
+﻿using Entity;
+using RepositoryFactory.RepositorysBase;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace RepositoryFactory.ServiceInterface
+{
+    public interface IAttachMentRepository : IRepository<AttachMentInfoEntity>
+    {
+    }
+}
