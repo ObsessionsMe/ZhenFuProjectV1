064c1b853ee078c08b95005f06e665f6ecdd87a2
86159 <86159@DESKTOP-EONNA4V>
2020-05-04 22:53:14 +0800

    。。。。

---
+++ WebApi/WebUI/Controllers/Client/AttachmentController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLogic.ClientService;
+using Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RepositoryFactory.ServiceInterface;
+using WebUI.App_Start;
+
+namespace WebUI.Controllers.Client
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AttachmentController : BaseControllers
+    {
+        private IAttachMentRepository attachMentRepository;
+        public AttachmentController(IAttachMentRepository _attachMentRepository)
+        {
+            attachMentRepository = _attachMentRepository;
+        }
+        /// <summary>
+        ///  获取附件列表
+        /// </summary>
+        /// <returns></returns>
+        [Route("GetAttachmentListByType")]
+        public ActionResult GetAttachmentListByType(int type)
+        {
+            //获取轮播图和产品列表
+            AttachmentService servers = new AttachmentService(attachMentRepository);
+            var data = servers.FindAttachMentList(type);
+            if (data == null)
+            {
+                return Json(new AjaxResult { state = ResultType.error.ToString(), message = "附件数据为空", data = "" });
+            }
+            return Json(new AjaxResult { state = ResultType.success.ToString(), message = "获取数据成功", data = data });
+        }
+    }
+}
