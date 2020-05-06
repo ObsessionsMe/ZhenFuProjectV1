3c4f771d25eabf3549f7550e9c81b0e4686a08ff
xiapeng <1448525265@qq.com>
2020-05-07 02:08:48 +0800

    。。。。

--- VueManage/src/lib/url.js
+++ VueManage/src/lib/url.js
@@ -1,6 +1,6 @@
 
 //const host = "http://121.36.74.193/"
-const  host = process.env.NODE_ENV === "development" ?  "https://localhost:44380/":"/"; 
+const  host = process.env.NODE_ENV === "development" ?  "https://localhost:44380/":"/shop.api/"; 
 console.log("process.env.NODE_ENV ",process.env.NODE_ENV); 
 //设置请求路径
 let url = {
