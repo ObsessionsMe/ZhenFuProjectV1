e4ff978da2f6e0887fae8c5f421c4a3f0ee528de
86159 <86159@DESKTOP-EONNA4V>
2020-05-14 20:40:41 +0800

    。。。。。

---
+++ WebApi/Repository/ServiceInterface/IUserBasePorintsRecordRepository.cs
@@ -0,0 +1,12 @@
+﻿using Entity;
+using RepositoryFactory.RepositorysBase;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.ServiceInterface
+{
+    public interface IUserBasePorintsRecordRepository : IRepository<UserBasePorintsRecordEntity>
+    {
+    }
+}
