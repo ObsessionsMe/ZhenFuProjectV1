c042c288bfb45e4363aa7b45f23e068184d5f845
86159 <86159@DESKTOP-EONNA4V>
2020-05-14 20:39:58 +0800

    。。。。

---
+++ WebApi/Repository/RepositoryService/UserBasePorintsRecordRepository.cs
@@ -0,0 +1,18 @@
+﻿using Entity;
+using Infrastructure.DBContext;
+using Repository.ServiceInterface;
+using RepositoryFactory.RepositorysBase;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.RepositoryService
+{
+    public class UserBasePorintsRecordRepository : Repository<UserBasePorintsRecordEntity>, IUserBasePorintsRecordRepository
+    {
+        public UserBasePorintsRecordRepository(ZFDBContext _dbcontext) : base(_dbcontext)
+        {
+
+        }
+    }
+}
