9c96d561beecdd9009f46e3e7dc7d6f4f1de6e5c
TianPeng <peng.tian@topevery.com>
2020-07-08 02:00:41 +0800

    111

---
+++ WebApi/UtilitieEntity/VipProductDetail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UtilitieEntity
+{
+    public class VipProductDetailModel
+    {
+        public int ProductPorints { get; set; }
+        public int TreamPorints { get; set; }
+        public int HoldingDays { get; set; }
+        public bool IsAgency { get; set; }
+    }
+}
